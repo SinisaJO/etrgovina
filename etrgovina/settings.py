cb80efdfa66841309b1c9ac1df4d2da3ae7aca3c
SinisaJO <73884960+SinisaJO@users.noreply.github.com>
2021-02-25 18:48:58 +0100

    data

--- etrgovina/settings.py
+++ etrgovina/settings.py
@@ -97,6 +97,8 @@ LOGGING = {
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+import psycopg2
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
