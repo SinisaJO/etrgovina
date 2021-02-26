5006a9b52dcd2970acfa5f177ec35d96295f4692
SinisaJO <73884960+SinisaJO@users.noreply.github.com>
2021-02-26 11:39:40 +0100

    data

--- etrgovina/settings.py
+++ etrgovina/settings.py
@@ -78,21 +78,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'etrgovina.wsgi.application'
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['console'],
-             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
-        },
-    },
-}
+
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
