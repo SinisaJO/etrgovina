d61c1cda97fb54b98c58b26682e62200557c4597
SinisaJO <73884960+SinisaJO@users.noreply.github.com>
2021-02-27 15:23:04 +0100

    data

--- etrgovina/settings.py
+++ etrgovina/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '*-jgp!^x%=$gy09oxw48gdxs$0z+jccyhla4ndllkh++_*0r%m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'etrgovina.herokuapp.com', '127.0.0.1'
